03561725c12f4774466533c721edf4343ede0b05
wxing <wxing2008666@126.com>
2020-08-19 17:26:49 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setuptools.setup(name='py_test',
+      version='0.01',
+      description='test install myself package',
+      url='https://github.com/wxing2008666/py_test',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
